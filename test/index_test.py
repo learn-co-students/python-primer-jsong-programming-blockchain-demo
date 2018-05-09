a758d0e6029f65d26f05023980cf78272bb1cbc4
adamjonas <jonas@flatironschool.com>
2018-05-08 21:19:46 -0400

    change import

--- test/index_test.py
+++ test/index_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from ipynb.fs.full.index import (str_to_bytes,
+from index import (str_to_bytes,
 bytes_to_str, little_endian_to_int, int_to_little_endian)
 
 class HelperTest(TestCase):
